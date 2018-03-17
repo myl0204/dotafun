78e982884055708135eaa3a95174b80713582b40
miao <yunliangmiao@gmail.com>
2018-03-17 21:36:08 +0800

    将部分数据抽离到本地，方便开发

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,41 @@
-var path = require('path')
+const path = require('path')
+const express = require('express')
+const apiRouter = express.Router()
+const playerData = require('./mockApi/playerdata.json')
+const heroesData = require('./mockApi/heroesdata.json')
+const matchesData = require('./mockApi/matchesdata.json')
+const totalsData = require('./mockApi/totalsdata.json')
 function resolve (dir) {
-  console.log(__dirname)
   return path.join(__dirname, dir)
 }
+
+
 module.exports = {
+  devServer: {
+    before(app) {
+      apiRouter.get('/playerData', (req, res) => {
+        res.json({
+          data: playerData
+        })
+      })
+      apiRouter.get('/heroesData', (req, res) => {
+        res.json({
+          data: heroesData
+        })
+      })
+      apiRouter.get('/matchesData', (req, res) => {
+        res.json({
+          data: matchesData
+        })
+      })
+      apiRouter.get('/totalsData', (req, res) => {
+        res.json({
+          data: totalsData
+        })
+      })
+      app.use('/api', apiRouter)
+    }
+  },
   chainWebpack: config => {
     config.resolve.alias
       .set('@@', resolve('src/components'))
