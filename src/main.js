7d9366bc4eb24ef1c228879bb56a512a5d60e738
miao <yunliangmiao@gmail.com>
2018-03-18 23:43:56 +0800

    组件的共有样式，覆盖elment原生样式

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import ElementUI from 'element-ui'
 import './common/scss/variables.scss'
 import Axios from 'axios'
+// eslint-disable-next-line
+import scss from './common/scss/public.scss'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.prototype.$http = Axios
