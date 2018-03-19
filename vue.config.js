003a0bf7f90965b12cc2479cfdaa50049ee73813
miao <yunliangmiao@gmail.com>
2018-03-19 17:44:09 +0800

    修改webpack配置

--- vue.config.js
+++ vue.config.js
@@ -39,5 +39,9 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       .set('@@', resolve('src/components'))
+    if (process.env.NODE_ENV === 'production') {
+      config.output
+        .publicPath('/dotafun')
+    }
   }
 }
