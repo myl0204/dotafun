1c7ee8ff6519e22f9eda24307c4576980a97a293
myl cl <yunliangmiao@gmail.com>
2018-03-19 09:21:02 +0000

    rebuild pages

--- src/common/util.js
+++
@@ -1,9 +0,0 @@
-/**
- * 简单的四舍五入
- * @param {Number} number 要四舍五入的数
- * @param {Number} precision 小数点后保留几位
- */
-export function precisionRound(number, precision) {
-  const factor = Math.pow(10, precision)
-  return Math.round(number * factor) / factor
-}
